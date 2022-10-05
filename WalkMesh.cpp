61bf6c7c7a264ae5523a41521334855a602baba1
Henry Du <henrytong98@gmail.com>
2022-10-05 02:01:07 -0400

    Remove unused variable

--- WalkMesh.cpp
+++ WalkMesh.cpp
@@ -244,7 +244,6 @@ bool WalkMesh::cross_edge(WalkPoint const &start, WalkPoint *end_, glm::quat *ro
 	auto &rotation = *rotation_;
 
 	assert(start.weights.z == 0.0f); //*must* be on an edge.
-	glm::uvec2 edge = glm::uvec2(start.indices);
 
 	//check if 'edge' is a non-boundary edge:
 	auto f = next_vertex.find(glm::uvec2(start.indices.y, start.indices.x));
